95f0277bdd10c389c362bfbdbf0dd935a341c2f8
veyseltosun <postalarim1923@gmail.com>
2022-08-29 03:45:22 +0300

    starting

---
+++ todo-app/src/components/Button.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { useState, useRef } from "react";
+
+const Button = () =>{
+    const[todos, setTodos] = useState();
+    <input type="text" />
+    <button ></button>
+}
